839696e3e5550c38d701b262aa95295a85ffddb6
renanpmiranda <renanpmiranda5@gmail.com>
2022-09-14 13:28:21 -0300

    a017_exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,25 @@
+// function imprimeNome(nome){
+//     console.log(`Olá, ${nome}!`)
+// }
+
+// imprimeNome('Renan')
+
+// function tabuadaSeis(){
+//     for(i = 1; i <= 10; i++){
+//         console.log(`6 x ${i} = ${6*i}`)
+//     }
+// }
+
+// tabuadaSeis()
+
+const imprimeNome = (nome) => console.log(`Olá, ${nome}!`)
+
+imprimeNome('Renan')
+
+const tabuadaSeis = function(){
+    for(i = 1; i <= 10; i++){
+        console.log(`6 x ${i} = ${6*i}`)
+    }
+}
+
+tabuadaSeis()
